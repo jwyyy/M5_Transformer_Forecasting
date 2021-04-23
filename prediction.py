ccc1d640fa98cbeb845c01142e927737882d2ee5
Junwen <junwen@MBP-YAO.local>
2021-04-23 05:25:48 -0700

    added model.to(device)

--- prediction.py
+++ prediction.py
@@ -3,7 +3,7 @@ import numpy as np
 from trainOps import DataLoader, compute_loss, get_mask, compute_prediction_loss
 
 # set up GPU
-device = torch.device("cpu")
+device = torch.device("cuda:1")
 
 # model configuration
 CONST_LEN = 28
@@ -15,10 +15,12 @@ model = checkpoint["model"]
 model.load_state_dict(checkpoint["state_dict"])
 for parameter in model.parameters():
     parameter.requires_grad = False
+# send model to GPU
+model.to(device)
 model.eval()
 
 # set random seed if used a non default value
-dataLoader = DataLoader('valid_X.csv', batch_size=512, cat_exist=False, split=(90, 5, 5))
+dataLoader = DataLoader('valid_X.csv', batch_size=512, cat_exist=False, split=(90, 2, 2))
 src_mask, tar_mask = get_mask(4 * CONST_LEN, random=False)
 # send src_mask, tar_mask to GPU
 src_mask, tar_mask = src_mask.to(device), tar_mask.to(device)
@@ -46,6 +48,11 @@ for i, (cat, x, y) in enumerate(dataLoader.get_original_test_batch()):
     flag = torch.round(pred_y[i]) > 0
     y_ = flag * torch.ceil(pred_y[i])
     loss_pred.append(compute_prediction_loss(y_, y, tar_mask).item())
+    if i % 24 == 0:
+        print("A few checks ...")
+        print(y[1, -CONST_LEN:])
+        print(y_[1, -CONST_LEN:])
+        print("============================")
 
 print("Original test dataset loss : ", np.mean(loss_pred))
 
